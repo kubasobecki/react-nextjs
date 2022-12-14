51293979a9e195eb6033c9cc0d58ff35cb4f6435
Kuba Sobecki <gosustyle@gmail.com>
2022-12-14 17:57:28 +0100

    Add some nested pages with dynamic parameters

---
+++ pages/news/index.js
@@ -0,0 +1,13 @@
+function NewsPage() {
+    return (
+        <>
+            <h1>The News Page</h1>;
+            <ul>
+                <li>NextJS is a great framework</li>
+                <li>Some other article</li>
+            </ul>
+        </>
+    );
+}
+
+export default NewsPage;
