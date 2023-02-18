a787876c4184447dae1aafb17070e34857d63bd5
Kuba Sobecki <gosustyle@gmail.com>
2023-02-18 21:33:42 +0100

    Add general layout and navigation

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
 const NewMeetup = () => {
-    return <div>NewMeetup</div>;
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData);
+    }
+
+    return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
 
 export default NewMeetup;
